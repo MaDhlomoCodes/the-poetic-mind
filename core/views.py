5cda08716c1d95ff15f201ad22279484495a68f4
MaDhlomoCodes <prescillamkhabela@gmail.com>
2025-08-30 15:57:47 +0200

    Added the code to views.py in core

--- core/views.py
+++ core/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Poem, Comment, Like, Bookmark
+from .serializers import PoemSerializer, CommentSerializer, LikeSerializer, BookmarkSerializer
 
-# Create your views here.
+class PoemViewSet(viewsets.ModelViewSet):
+    queryset = Poem.objects.all().order_by('-created_at')
+    serializer_class = PoemSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all().order_by('-created_at')
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+class LikeViewSet(viewsets.ModelViewSet):
+    queryset = Like.objects.all()
+    serializer_class = LikeSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+class BookmarkViewSet(viewsets.ModelViewSet):
+    queryset = Bookmark.objects.all()
+    serializer_class = BookmarkSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
